3d49d7c8bda734713f0e1f881fcddf6fdd7b7a0e
edersonff <=>
2024-03-19 18:24:06 -0300

    fix: product entity case in import

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Service } from 'class/service/entity.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Product } from './entities/Product.entity';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService extends Service<Product> {
