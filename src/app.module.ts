32872dbf35070b4239ec7e2f90d214c608fda619
edersonff <=>
2024-03-18 23:09:59 -0300

    feat: using external db source options

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,34 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { CategoryModule } from './category/category.module';
 import { VideoModule } from './video/video.module';
 import { SocialModule } from './social/social.module';
 import { ProductModule } from './product/product.module';
-import { Video } from './video/entities/video.entity';
-import { Category } from './category/entities/category.entity';
-import { Social } from './social/entities/social.entity';
-import { Product } from './product/entities/product.entity';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksModule } from './tasks/tasks.module';
+import { dataSourceOptions } from './db/data-source';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: (process.env.DB_TYPE as 'mysql') || 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || 'root',
-      entities: [User, Video, Category, Social, Product],
-      database: process.env.DB_NAME || 'sociable-ai',
-      synchronize: false,
-      logging: true,
-      ssl: process.env.DB_SSL === 'true',
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     AuthModule,
     UsersModule,
     CategoryModule,
@@ -40,6 +26,7 @@ import { TasksModule } from './tasks/tasks.module';
     SocialModule,
     ProductModule,
     TasksModule,
+    SeedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
