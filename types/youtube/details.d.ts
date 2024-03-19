16c903c75d5a6e63833fff03c57b0f3cbc4a6cf8
edersonff <=>
2024-03-18 23:06:56 -0300

    feat: types folder

---
+++ types/youtube/details.d.ts
@@ -0,0 +1,19 @@
+export type VideoDetails = {
+  videoId: string;
+  title: string;
+  lengthSeconds: string;
+  keywords: string[];
+  channelId: string;
+  isOwnerViewing: boolean;
+  shortDescription: string;
+  isCrawlable: boolean;
+  thumbnail: {
+    thumbnails: {
+      url: string;
+      width: number;
+      height: number;
+    }[];
+  };
+  allowRatings: boolean;
+  viewCount: string;
+};
