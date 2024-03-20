5a15cc29b446de55a9f2837c34a11649f1418ad1
edersonff <=>
2024-03-20 18:35:18 -0300

    feat: auth guard verification

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,37 +6,55 @@ import {
   Patch,
   Param,
   Delete,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto, @Request() req) {
+    this.productService.setUserId(req.user.id);
+
     return this.productService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
+  findAll(@Request() req) {
+    this.productService.setUserId(req.user.id);
+
     return this.productService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string, @Request() req) {
+    this.productService.setUserId(req.user.id);
+
     return this.productService.findOne({ id: +id });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+    @Request() req,
+  ) {
+    this.productService.setUserId(req.user.id);
+
     return this.productService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string, @Request() req) {
+    this.productService.setUserId(req.user.id);
+
     return this.productService.remove(+id);
   }
 }
--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -6,12 +6,18 @@ import {
   // Patch,
   Param,
   Delete,
+  Request,
+  Patch,
+  UseGuards,
 } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { CreateVideoDto } from './dto/create-video.dto';
 // import { UpdateVideoDto } from './dto/update-video.dto';
 import { CategoryService } from 'src/category/category.service';
+import { UpdateVideoDto } from './dto/update-video.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('video')
 export class VideoController {
   constructor(
@@ -20,7 +26,13 @@ export class VideoController {
   ) {}
 
   @Post()
-  async create(@Body() { link }: CreateVideoDto) {
+  async create(
+    @Body()
+    { link, category, originalLanguage, targetLanguage }: CreateVideoDto,
+    @Request() req,
+  ) {
+    this.videoService.setUserId(req.user.id);
+
     const {
       title,
       keywords,
@@ -32,32 +44,79 @@ export class VideoController {
 
     const tags = keywords.join(',');
 
+    const categoryFound = await this.categoryService.findOneOrFail({
+      where: {
+        name: category,
+      },
+    });
+
     return this.videoService.create({
       name: title,
       tags,
       description: shortDescription,
       image: thumbnail,
       link,
+      originalLanguage,
+      targetLanguage,
+      category: categoryFound,
     });
   }
 
   @Get()
-  findAll() {
+  findAll(@Request() req) {
+    this.videoService.setUserId(req.user.id);
     return this.videoService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string, @Request() req) {
+    this.videoService.setUserId(req.user.id);
+
     return this.videoService.findOne({ id: +id });
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateVideoDto: UpdateVideoDto) {
-  //   return this.videoService.update(+id, updateVideoDto);
-  // }
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body()
+    { category, link, originalLanguage, targetLanguage }: UpdateVideoDto,
+    @Request() req,
+  ) {
+    this.videoService.setUserId(req.user.id);
+
+    const {
+      title,
+      keywords,
+      shortDescription,
+      thumbnail: { thumbnails },
+    } = await this.videoService.youtubeVideoDetails(link);
+
+    const thumbnail = thumbnails[thumbnails.length - 1].url;
+
+    const tags = keywords.join(',');
+
+    const categoryFound = await this.categoryService.findOneOrFail({
+      where: {
+        name: category,
+      },
+    });
+
+    return this.videoService.update(+id, {
+      name: title,
+      tags,
+      description: shortDescription,
+      image: thumbnail,
+      link,
+      originalLanguage,
+      targetLanguage,
+      category: categoryFound,
+    });
+  }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string, @Request() req) {
+    this.videoService.setUserId(req.user.id);
+
     return this.videoService.remove(+id);
   }
 }
