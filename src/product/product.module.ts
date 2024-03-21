bb1f828d4f66b05312ee81e535c88bb3f0fffa01
edersonff <=>
2024-03-20 22:07:21 -0300

    fix: product module category import

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,9 +3,10 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { Product } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CategoryModule],
   controllers: [ProductController],
   providers: [ProductService],
 })
