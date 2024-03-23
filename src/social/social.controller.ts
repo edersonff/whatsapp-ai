f44c50893d31bcd419ea9489287e16c56a88bc9c
edersonff <=>
2024-03-23 00:35:58 -0300

    feat: titkok handler social

--- src/social/social.controller.ts
+++ src/social/social.controller.ts
@@ -13,6 +13,7 @@ import { SocialService } from './social.service';
 import { CreateSocialDto } from './dto/create-social.dto';
 import { UpdateSocialDto } from './dto/update-social.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import axios from 'axios';
 
 @UseGuards(AuthGuard)
 @Controller('social')
@@ -24,6 +25,38 @@ export class SocialController {
     this.socialService.setUserId(req.user.id);
 
     return this.socialService.create(createSocialDto);
+  }
+
+  @Post('tiktok')
+  async tiktok(@Body() body, @Request() req) {
+    const findTiktok = await this.socialService.findOne({
+      type: 'tiktok',
+      user: {
+        id: req.user.id,
+      },
+    });
+
+    const {
+      data: { refresh_token },
+    } = await axios.post('https://open.tiktokapis.com/v2/oauth/token/', {
+      grant_type: 'authorization_code',
+      client_key: process.env.TIKTOK_CLIENT_KEY,
+      client_secret: process.env.TIKTOK_CLIENT_SECRET,
+      code: body.code,
+      redirect_uri: process.env.TIKTOK_REDIRECT_URI,
+    });
+
+    if (findTiktok) {
+      return this.socialService.update(findTiktok.id, {
+        type: 'tiktok',
+        token: refresh_token,
+      });
+    }
+
+    return this.socialService.create({
+      type: 'tiktok',
+      token: refresh_token,
+    });
   }
 
   @Get()
