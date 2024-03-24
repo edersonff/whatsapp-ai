89f1575314a59f8d20ae38e83c083228425e80bd
edersonff <=>
2024-03-23 21:42:53 -0300

    feat: importing PostModule and CategoryModule in Tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,17 @@ import { VideoModule } from 'src/video/video.module';
 import { UsersModule } from 'src/users/users.module';
 import { TaskController } from './tasks.controller';
 import { ProductModule } from 'src/product/product.module';
+import { PostModule } from 'src/post/post.module';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [VideoModule, UsersModule, ProductModule],
+  imports: [
+    VideoModule,
+    UsersModule,
+    ProductModule,
+    PostModule,
+    CategoryModule,
+  ],
   controllers: [TaskController],
   exports: [TasksService],
   providers: [TasksService],
